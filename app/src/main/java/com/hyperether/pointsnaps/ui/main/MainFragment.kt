b15b233f8de43be65a9b7129097cf03c688fea3a
NikolaHadzic <nikola.hadzic@hotmail.com>
2020-04-16 10:26:01 +0200

    add animations

--- app/src/main/java/com/hyperether/pointsnaps/ui/main/MainFragment.kt
+++ app/src/main/java/com/hyperether/pointsnaps/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hyperether.pointsnaps.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -25,6 +27,14 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
+        buttonLoc.setOnClickListener { v->
+            findNavController().navigate(R.id.action_main_to_location)
+        }
+
+        buttonDes.setOnClickListener { v->
+            findNavController().navigate(R.id.action_main_to_description)
+        }
     }
 
 }
