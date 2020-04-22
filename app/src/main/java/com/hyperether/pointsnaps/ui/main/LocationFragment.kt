9a3be40b27fee868ae6b8a3e73a4fbc6364e860e
NikolaHadzic <nikola.hadzic@hotmail.com>
2020-04-22 12:25:00 +0200

    add map

--- app/src/main/java/com/hyperether/pointsnaps/ui/main/LocationFragment.kt
+++ app/src/main/java/com/hyperether/pointsnaps/ui/main/LocationFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 import com.hyperether.pointsnaps.R
+import kotlinx.android.synthetic.main.fragment_location.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class LocationFragment : Fragment() {
+class LocationFragment : Fragment(), OnMapReadyCallback {
 
     lateinit var viewModel: MainViewModel
 
@@ -27,6 +31,16 @@ class LocationFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(activity!!).get(MainViewModel::class.java)
+        val mapFragment : SupportMapFragment? =
+            parentFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap ?: return
+        with(googleMap) {
+
+        }
     }
 
 }
