e382ece49cc8f83bb9c1f1361b3b754cab197ff0
Petar Radisavljevic <petar.radisav@gmail.com>
2023-12-20 11:37:07 +0100

    In RegisterFragment migration from Kotlin synthetics to Jetpack view binding has been done.

--- app/src/main/java/com/hyperether/pointsnaps/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/hyperether/pointsnaps/ui/auth/RegisterFragment.kt
@@ -6,68 +6,70 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.hyperether.pointsnaps.R
+import com.hyperether.pointsnaps.databinding.FragmentRegisterBinding
 import com.hyperether.pointsnaps.ui.base.BaseFragment
 import com.hyperether.pointsnaps.utils.Utils
-import kotlinx.android.synthetic.main.fragment_register.*
 
 
 class RegisterFragment : BaseFragment() {
     // TODO: Rename and change types of parameters
 
-
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
     private lateinit var viewModel: AuthViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(activity!!).get(AuthViewModel::class.java)
+        viewModel = ViewModelProvider(activity!!)[AuthViewModel::class.java]
         setupObservers()
-        toolbar.setTitle(getString(R.string.location))
-        toolbar.setNavigationIcon(resources.getDrawable(R.drawable.ic_navigation_icon))
-        toolbar.setNavigationOnClickListener {
-            findNavController().popBackStack()
-        }
-        signinTxt.setOnClickListener { v ->
-            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
-        }
-        signupBtn.setOnClickListener {
-            if (!Utils.fieldEmptyValidator(arrayOf(emailET, passwordET, codeET, usernameET))) {
-                createToast(getString(R.string.fill_fields))
-            } else if (!Utils.fullNameValidator(usernameET)) {
-                createToast(getString(R.string.full_name_validation_toast))
-            } else {
-                progressBar.visibility = VISIBLE
-                viewModel.registerUser(
-                    emailET.text.toString(),
-                    usernameET.text.toString().split(" ")[0],
-                    usernameET.text.toString().split(" ")[1],
-                    passwordET.text.toString(),
-                    codeET.text.toString()
-                ) {
-                    progressBar.visibility = GONE
+        binding.apply {
+            toolbar.title = getString(R.string.location)
+            toolbar.navigationIcon = resources.getDrawable(R.drawable.ic_navigation_icon, null)
+            toolbar.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+            signinTxt.setOnClickListener {
+                findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+            }
+            signupBtn.setOnClickListener {
+                if (!Utils.fieldEmptyValidator(arrayOf(emailET, passwordET, codeET, usernameET))) {
+                    createToast(getString(R.string.fill_fields))
+                } else if (!Utils.fullNameValidator(usernameET)) {
+                    createToast(getString(R.string.full_name_validation_toast))
+                } else {
+                    progressBar.visibility = VISIBLE
+                    viewModel.registerUser(
+                        emailET.text.toString(),
+                        usernameET.text.toString().split(" ")[0],
+                        usernameET.text.toString().split(" ")[1],
+                        passwordET.text.toString(),
+                        codeET.text.toString()
+                    ) {
+                        progressBar.visibility = GONE
+                    }
                 }
             }
         }
     }
 
-    fun setupObservers() {
-        viewModel.error.observe(viewLifecycleOwner, Observer {
+    private fun setupObservers() {
+        viewModel.error.observe(viewLifecycleOwner) {
             createToast(it)
-        })
+        }
 
-        viewModel.registerUser.observe(viewLifecycleOwner, Observer {
+        viewModel.registerUser.observe(viewLifecycleOwner) {
             findNavController().popBackStack()
-        })
+        }
     }
 }
